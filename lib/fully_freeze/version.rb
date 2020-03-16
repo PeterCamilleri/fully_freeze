51a588ed4d9f217b258dfb1757325e2d5be0fc41
PeterCamilleri <peter.c.camilleri@gmail.com>
2020-03-16 19:08:11 -0400

    New version 0.1.1 for issue #1

--- lib/fully_freeze/version.rb
+++ lib/fully_freeze/version.rb
@@ -2,7 +2,7 @@
 
 # A deep, recursive freeze. Adds the fully_freeze and fully_frozen? methods.
 module FullyFreeze
-  VERSION = "0.1.0".freeze
+  VERSION = "0.1.1".freeze
   
   DESCRIPTION = "A deep, recursive freeze and freeze tester.".freeze
 end
