6bf764d3ea223a44b4d9abfd570c796b64e9075d
PeterCamilleri <peter.c.camilleri@gmail.com>
2020-03-28 14:43:07 -0400

    Removed dependency on minitest_visible for issue #2

--- test/fully_freeze_test.rb
+++ test/fully_freeze_test.rb
@@ -1,7 +1,6 @@
 require_relative '../lib/fully_freeze'
 gem              'minitest'
 require          'minitest/autorun'
-require          'minitest_visible'
 
 class LoremIpsum
 
@@ -18,9 +17,6 @@ end
 Sample = Struct.new(:a, :b, :c)
 
 class FullyFreezeTest < Minitest::Test
-
-  #Track mini-test progress.
-  include MinitestVisible
 
   def test_that_it_has_a_version_number
     refute_nil ::FullyFreeze::VERSION
